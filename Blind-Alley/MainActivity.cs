590dde0ba41a4a676b8ed39d9973ce9707325893
SJCRPV <sjcrpv.games@hotmail.com>
2018-04-29 15:16:04 +0100

    Fixed onClick functions I also moved the entire project up a level in the directory because there was no reason for it to be where it was.

---
+++ Blind-Alley/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using Android.Views;
+using System;
+
+namespace Blind_Alley
+{
+    [Activity(Label = "Blind_Alley", MainLauncher = true)]
+    public class MainActivity : Activity
+    {
+        View mainView;
+        Button forwardBtn;
+        Button leftBtn;
+        Button rightBtn;
+        Button backwardBtn;
+        Button interactBtn;
+
+        private void setListeners()
+        {
+            forwardBtn.Click += (sender, e) =>
+            {
+                Console.WriteLine("Forward!");
+            };
+
+            leftBtn.Click += (sender, e) =>
+            {
+                Console.WriteLine("Turn Left!");
+            };
+
+            interactBtn.Click += (sender, e) =>
+            {
+                 Console.WriteLine("Wut?");
+            };
+
+            rightBtn.Click += (sender, e) =>
+            {
+                Console.WriteLine("Turn Right!");
+            };
+
+            backwardBtn.Click += (sender, e) =>
+            {
+                Console.WriteLine("Backward!");
+            };
+        }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+            mainView = Window.DecorView;
+            forwardBtn = mainView.FindViewById<Button>(Resource.Id.buttonForward);
+            leftBtn = mainView.FindViewById<Button>(Resource.Id.buttonLeft);
+            rightBtn = mainView.FindViewById<Button>(Resource.Id.buttonRight);
+            interactBtn = mainView.FindViewById<Button>(Resource.Id.buttonInteract);
+            backwardBtn = mainView.FindViewById<Button>(Resource.Id.buttonBackward);
+
+            setListeners();
+        }
+    }
+}
+
