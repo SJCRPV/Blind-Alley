e3df2eaebce3cb9b45a1f33301c12988edf3de71
SJCRPV <sjcrpv.games@hotmail.com>
2018-04-29 17:49:38 +0100

    Added Map, Player and Monster classes

---
+++ Blind-Alley/Map.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Blind_Alley
+{
+    class Map
+    {
+        Random rand;
+        bool[,] map;
+        int[] objectiveCoords;
+        int[] playerCoords;
+        int[] monsterCoords;
+        int mapWidth;
+        int mapHeight;
+
+        private int[] getRandomCoord()
+        {
+            return new int[] { rand.Next(mapWidth), rand.Next(mapHeight) };
+        }
+
+        private void generateMap()
+        {
+
+        }
+
+        public Map(int nMapWidth, int nMapHeight)
+        {
+            rand = new Random();
+            map = new bool[nMapWidth, nMapHeight];
+            generateMap();
+            playerCoords = new int[] { 0, 0 };
+            objectiveCoords = getRandomCoord();
+            monsterCoords = (int[])objectiveCoords.Clone();
+        }
+    }
+}
